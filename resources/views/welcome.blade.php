fba2c63875cb0d0294892312e83d21b2f52b5caf
Dio Hakam Dalu <diohakamdalu@student.telkomuniversity.ac.id>
2021-06-14 23:39:41 +0700

    Try: more AR on sharing locale server

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,9 +12,12 @@
 
 
 <body style="margin : 0px; overflow: hidden;">
-    <a-scene embedded arjs>
+    <a-scene embedded arjs="detectionMode:mono_and_matrix;matrixCodeType:3x3_PARITY65" >
         <a-marker preset="hiro">
-            <a-entity gltf-model="{{ asset('lieutenantHead/a.gltf') }}" position="0 -10 0"></a-entity>
+            <a-box color="red" material="opacity:0.5" scale="0.5 0.5 0.5"></a-box>
+        </a-marker>
+        <a-marker type="pattern" url="/pattern-telkom.patt">
+            <a-box color="red" material="opacity:0.5" scale="0.5 0.5 0.5"></a-box>
         </a-marker>
         <a-entity camera>
             <a-cursor></a-cursor>
@@ -23,3 +26,25 @@
 </body>
 
 </html>
+{{-- <!doctype HTML>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
+    </head>
+    <script src="https://aframe.io/releases/0.8.0/aframe.min.js"></script>
+    <script src="https://rawgit.com/jeromeetienne/AR.js/master/aframe/build/aframe-ar.min.js"></script>
+    <script src="https://rawgit.com/donmccurdy/aframe-extras/master/dist/aframe-extras.loaders.min.js"></script>
+    <body style='margin : 0px; overflow: hidden;'>
+        <!-- kita akan menambahkan detectionMode dan matrixCodeType untuk memberitahu ke AR.js untuk mengenali kode unik -->
+        <a-scene embedded arjs="detectionMode:mono_and_matrix;matrixCodeType:3x3_PARITY65" >
+
+
+         <a-marker preset="barcode" value="5">
+            <a-box color="red" material="opacity:0.5" scale="0.5 0.5 0.5"></a-box>
+        </a-marker>
+
+        <!-- gunakan <a-entity camera> untuk mendukung tanda yang ganda, selain itu, gunakan <a-marker-camera> sebagai pengganti </a-marker> -->
+        <a-entity camera></a-entity>
+        </a-scene>
+    </body>
+</html> --}}
--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('auth.login');
-});
+
 
 Auth::routes();
 
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
